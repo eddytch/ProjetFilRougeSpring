cc333a2ef4c1a080a2fa1faa294e5044fb181241
dimitri lanoe <dimitrilanoe35@gmail.com>
2016-04-27 10:52:01 +0200

    Ajout du model

---
+++ src/main/java/fr/iocean/application/model/Auteur.java
@@ -0,0 +1,94 @@
+package fr.iocean.application.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.List;
+
+@Entity
+public class Auteur{
+
+    @Id
+    @GeneratedValue
+    private long id ;
+
+    @Column
+	@NotEmpty
+    private String nom ;
+
+    @Column
+    private String prenom ;
+
+    @OneToMany(mappedBy = "auteurMedia")
+    private List<Media> listeMedias ;
+
+    public Auteur(){
+
+    }
+
+    public Auteur(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+	/**
+	 * Affichage client
+	 */
+	public String toString() {
+		return "Auteur (id =" + getId() + "," + getNom() + "," + getPrenom() + ")";
+	}
+
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Auteur auteur = (Auteur) o;
+
+        return id == auteur.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public List<Media> getListeMedias() {
+        return listeMedias;
+    }
+
+    public void setListeMedias(List<Media> listeMedias) {
+        this.listeMedias = listeMedias;
+    }
+
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/fr/iocean/application/model/Media.java
@@ -0,0 +1,94 @@
+package fr.iocean.application.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by utilisateur on 23/03/2016.
+ */
+@Entity
+public class Media {
+
+    public enum Type {Livre,CD,DVD}
+
+    @Id
+    @GeneratedValue
+    private long id ;
+
+    @Column
+	@NotEmpty
+    private String titre ;
+
+	@NotNull
+    @Enumerated(EnumType.STRING)
+    private Type type ;
+
+	@NotNull
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Auteur auteurMedia ;
+
+	@OneToMany(mappedBy = "mediaLoue")
+	private List<Emprunt> mediaEmprunte;
+
+    public Media(){
+
+    }
+
+    public Media(String titre, Auteur auteurMedia, Type type) {
+        this.titre = titre;
+        this.auteurMedia = auteurMedia;
+        this.type = type;
+    }
+
+	/**
+	 * Affichage client
+	 */
+	public String toString() {
+		return "Media (id =" + getId() + "," + getTitre() + "," + getType() + "," + getAuteurMedia()  + ")";
+	}
+
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Auteur getAuteurMedia() {
+        return auteurMedia;
+    }
+
+    public void setAuteurMedia(Auteur auteurMedia) {
+        this.auteurMedia = auteurMedia;
+    }
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<Emprunt> getMediaEmprunte() {
+		return mediaEmprunte;
+	}
+
+	public void setMediaEmprunte(List<Emprunt> mediaEmprunte) {
+		this.mediaEmprunte = mediaEmprunte;
+	}
+}
